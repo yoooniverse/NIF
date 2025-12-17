cdcbcb679bd5e8c6700cdb8eabecd3928560ca35
yoooniverse <yooon0322@gmail.com>
2025-12-17 23:29:14 +0900

    빌드 에러 해결

--- app/signup/[[...sign-up]]/page.tsx
+++ app/signup/[[...sign-up]]/page.tsx
@@ -46,7 +46,6 @@ export default function SignupPage() {
           <SignUp
             path="/signup"
             routing="path"
-            signInUrl="/login"
             forceRedirectUrl="/onboarding/interests"
             signInForceRedirectUrl="/dashboard"
             appearance={{
